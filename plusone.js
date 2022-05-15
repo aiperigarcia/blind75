1d12bcf6ac2e056d73401d32d10cb7635fa2cdf0
Aiperi Eldar <aiperieldar.dev@gmail.com>
2022-05-14 21:39:18 -0400

    solution

--- plusone.js
+++ plusone.js
@@ -23,3 +23,18 @@ Thus, the result should be [4,3,2,2].
 
 */
 
+var plusOne = function(digits) {
+    
+    for (let i=digits.length-1; i>=0; i--) {
+        if (digits[i]!=9) {
+            digits[i]+=1;
+            return digits;
+        }
+        else {
+            digits[i]=0;
+        }
+    }
+    
+    if (digits[0]===0) digits.unshift(1);
+    return digits;
+};
