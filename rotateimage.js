db48cf48d5af1f169e53275a206923ed5a448538
Aiperi Eldar <aiperieldar.dev@gmail.com>
2022-05-16 14:03:33 -0400

    sol

--- rotateimage.js
+++ rotateimage.js
@@ -11,3 +11,23 @@ Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 Output: [[7,4,1],[8,5,2],[9,6,3]]
 
 */
+
+var rotate = function(matrix){
+    var n = matrix.length;
+    
+    for (var i=0; i<n;i++){
+      for(var j=0; j<n; j++){
+        var tmp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = tmp;
+        if(i==j){
+          break;
+        }
+      }
+    }
+    
+    for (var k=0; k<n; k++){
+      matrix[k].reverse();
+    }
+  
+  };
