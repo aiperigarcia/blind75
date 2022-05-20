27c7e6589ece826113166f89533331837ead8a65
Aiperi Eldar <aiperieldar.dev@gmail.com>
2022-05-20 19:48:09 -0400

    sol

--- intersection.js
+++ intersection.js
@@ -16,3 +16,13 @@ Explanation: [9,4] is also accepted.
 
 */
 
+var intersect = function(nums1, nums2) {
+    return nums1.filter(num => {
+        let idx = nums2.indexOf(num)
+        if(idx > -1) {
+            nums2[idx]=null
+            return true
+        }
+    })
+    
+};
