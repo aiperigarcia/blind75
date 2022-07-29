fb0b75d3d0bdc55d9ba49012dc028154abb92a85
Aiperi Eldar <aiperieldar.dev@gmail.com>
2022-07-29 15:08:20 -0400

    new

--- duplicate.js
+++ duplicate.js
@@ -33,3 +33,15 @@ var containsDuplicate = function(nums) {
     }
     return false;
 };
+
+/*
+var containsDuplicate = function(nums) {
+    const newSet = new Set()
+    for( let i = 0; i < nums.length ; i ++){
+        if(newSet.has(nums[i])){
+            return true
+        }
+          newSet.add(nums[i]) 
+    } return false
+};
+ */
